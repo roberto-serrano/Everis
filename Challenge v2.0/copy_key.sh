b704ee47123db72da36e292821f62ed45cd22255
roberto-serrano <60835328+roberto-serrano@users.noreply.github.com>
2020-02-12 21:01:10 -0500

    Add files via upload

---
+++ Challenge v2.0/copy_key.sh
@@ -0,0 +1,9 @@
+mkdir /tmp/machine{1..4}
+cp -a /vagrant/.vagrant/machines/machine1/virtualbox/private_key /tmp/machine1
+chmod 600 /tmp/machine1/private_key
+cp -a /vagrant/.vagrant/machines/machine2/virtualbox/private_key /tmp/machine2
+chmod 600 /tmp/machine2/private_key
+cp -a /vagrant/.vagrant/machines/machine3/virtualbox/private_key /tmp/machine3
+chmod 600 /tmp/machine3/private_key
+cp -a /vagrant/.vagrant/machines/machine4/virtualbox/private_key /tmp/machine4
+chmod 600 /tmp/machine4/private_key
